f387ed401dd451980de09ce87681d9211c25a5be
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-05-19 11:36:46 +0200

    refactor(layoutWelcomeMobile): HTML injection using components created in TypeScript files

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
+import { showWelcomePage } from './welcome'; 
 
+// Ejecutamos la función de bienvenida de manera segura al cargar la página
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', showWelcomePage);
+} else {
+    showWelcomePage();
+}
---
+++ src/welcome.ts
@@ -0,0 +1,20 @@
+import logoUrl from '/src/assets/icons/Header.svg';
+
+export const showWelcomePage = () => {
+    const welcomePage = document.getElementById('insertWelcomePage');
+    
+    if (welcomePage){
+        welcomePage.innerHTML =    ` <div class="container-welcome">
+      <header>
+        <img src="${logoUrl}" alt="nombre de la aplicación: alumni" class="logo-welcome">
+      </header>
+      <section>
+        <p class="paragraph-hero">Connectant i empoderant a la nostra comunitat global d’alumnes</p>
+      </section>
+      <section class="join-button-welcome">
+        <a href="#" class="join-button">Uneix-te</a>
+      </section>
+    </div> 
+`;
+    }  
+}
