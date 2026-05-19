8e033ca1f9a20886228a7d5fa6266cc0ad44a628
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-05-19 23:31:43 +0200

    feat(layoutNavBar): Add CSS styles to layout navbar

--- src/nav-bar.ts
+++ src/nav-bar.ts
@@ -1,5 +1,5 @@
 import './nav-bar.css';
-import logoNavUrl from '/src/assets/icons/Header.svg';
+import logoUrl from '/src/assets/icons/Header.svg';
 import iconRegisterUrl from "/src/assets/icons/Navbar/register.svg";
 import iconProfileUrl from "/src/assets/icons/Navbar/profile.svg";
 
@@ -8,15 +8,15 @@ export const showNavBar = () => {
 
   if (navBar) {
     navBar.innerHTML = ` <header class="container-nav-bar">
-      <img src="${logoNavUrl}" alt="nombre de la aplicación: alumni" class="logo-nav-bar">
+      <img src="${logoUrl}" alt="nombre de la aplicación: alumni" class="logo-nav-bar">
       <ul class="navigation-menu" aria-label="Menú de navegación">
         <li><a href="#" aria-label="Ir a inicio">Inici</a></li>
         <li><a href="#" aria-label="Ir a red de exalumnos">Xarxa</a></li>
         <li><a href="#" aria-label="Ir a ofertas laborales">Oportunitas de feina</a></li>
       </ul>
       <div class="navigation-buttons" aria-label="Botones de navegación">
-        <a href="#" class="register-button"><img src="${iconRegisterUrl}" alt="Icono suscribirse">Apunta't</a>
-        <a href="#" class="profile-button"><img src="${iconProfileUrl}" alt="Icono editar perfil">Com et veuen?</a>
+        <a href="#" class="register-button button"><img src="${iconRegisterUrl}" alt="Icono suscribirse">Apunta't</a>
+        <a href="#" class="profile-button button"><img src="${iconProfileUrl}" alt="Icono editar perfil">Com et veuen?</a>
       </div>
     </header>
 `;
