614188728a06e2d5dd152b0c68e9dd465a78e132
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-05-20 17:53:20 +0200

    feat(layoutMainHomeLaptop): Add HTML structure to the main section of the homepage

---
+++ src/main-home-laptop.ts
@@ -0,0 +1,117 @@
+import "./main-home-laptop.css";
+import imgHero from "/src/assets/images/Image 28.png";
+import iconHat from "/src/assets/icons/Benefits/Card_1/Hat.svg";
+import iconBabbling from "/src/assets/icons/Benefits/Card_2/Babbling.svg";
+import iconPerson from "/src/assets/icons/Benefits/Card_3/person.svg";
+import imgJhoni from "/src/assets/images/Opinions/Card_1_3/JhoniTechnoSka.png";
+import imgEmma from "/src/assets/images/Opinions/Card_2/Emma.png";
+import iconStar from "/src/assets/icons/Opinions/Frame.svg";
+import iconBack from "/src/assets/icons/Opinions/Buttons/Back.png";
+import iconNext from "/src/assets/icons/Opinions/Buttons/Next.png";
+  
+export const showMainHomeLaptop = () => {
+  const mainHomeLaptop = document.getElementById('insertApp');
+
+  if (mainHomeLaptop) {
+    mainHomeLaptop.innerHTML = ` <main>
+    <section class="hero">
+      <h1>Benvingut, Alumni</h1>
+      <p>Connectant i empoderant a la nostra comunitat global d’alumnes</p>
+      <div class="buttons-hero">
+        <a class="button-register" href="#" aria-label="link a registro">Uneix-te</a>
+        <a class="button-info" href="#" aria-label="link a información sobre el proyecto">Mira que fem</a>
+      </div>
+      <img src="${imgHero}" alt="Reunión entre estudiantes">
+    </section>
+
+    <section class="benefits">
+      <h2>Què guanyes en formar-ne part?</h2>
+
+      <div class="benefits-cards">
+
+        <div class="benefit-card">
+          <img src="${iconHat}" alt="Icono red social de excompañeros">
+          <p>Comparteix i no perdis el contacte: Puja els teus moments importants, explica com va tot i queda amb els
+            companys. Una xarxa per estar més a prop.</p>
+          <a class="button-register-benefits" href="#" aria-label="link a registro">Apunta't ja</a>
+        </div>
+
+        <div class="benefit-card">
+          <img src="${iconBabbling}" alt="Icono discusiones">
+          <p>Participa en discussions: Intercanvia coneixements, punts de vista i opinions sobre temes que t'interessen.
+          </p>
+          <a class="button-register-benefits" href="#" aria-label="link a registro">Apunta't ja</a>
+        </div>
+
+        <div class="benefit-card">
+          <img src="${iconPerson}" alt="Icono conexiones personales">
+          <p>Xarxa Alumni: Connecta amb companys de promoció, fes noves amistats i crea records per durar tota la vida.
+          </p>
+          <a class="button-register-benefits" href="#" aria-label="link a registro">Apunta't ja</a>
+        </div>
+
+      </div>
+    </section>
+
+    <section class="opinions">
+      <h2>"T'ensenyem el que opinen els nostres súper-usuaris!”</h2>
+
+      <div class="opinions-cards">
+
+        <div class="opinion-card">
+          <img class="foto-opinion-card" src="${imgJhoni}" alt="Foto perfil Mikel">
+          <p class="user-name">Mikel</p>
+          <div class="rating">
+            <img src="${iconStar}" alt="Estrella de valoración">
+            <img src="${iconStar}" alt="Estrella de valoración">
+            <img src="${iconStar}${iconStar}">
+            <img src="${iconStar}" alt="Estrella de valoración">
+            <img src="${iconStar}" alt="Estrella de valoración">
+          </div>
+          <p class="opinion-paragraph">"Gràcies a IT Alumni vaig aconseguir la feina dels meus somnis en el món tech amb
+            el seu increïble programa de mentoria."</p>
+        </div>
+
+        <div class="opinion-card">
+          <img class="foto-opinion-card" src="${imgEmma}" alt="Foto perfil Emma">
+          <p class="user-name">Emma</p>
+          <div class="rating">
+            <img src="${iconStar}" alt="Estrella de valoración">
+            <img src="${iconStar}" alt="Estrella de valoración">
+            <img src="${iconStar}" alt="Estrella de valoración">
+            <img src="${iconStar}" alt="Estrella de valoración">
+            <img src="${iconStar}" alt="Estrella de valoración">
+          </div>
+          <p class="opinion-paragraph">Participa en discussions: Intercanvia coneixements, punts de vista i opinions
+            sobre
+            temes que t'interessen.</p>
+        </div>
+
+        <div class="opinion-card">
+          <img class="foto-opinion-card" src="${imgJhoni}" alt="Foto perfil Laia">
+          <p class="user-name">Laia</p>
+          <div class="rating">
+            <img src="${iconStar}" alt="Estrella de valoración">
+            <img src="${iconStar}" alt="Estrella de valoración">
+            <img src="${iconStar}" alt="Estrella de valoración">
+            <img src="${iconStar}" alt="Estrella de valoración">
+            <img src="${iconStar}" alt="Estrella de valoración">
+          </div>
+          <p class="opinion-paragraph">"IT Alumni em va donar les eines i l’autoestima per fer realitat el meu somni
+            d’emprendre."</p>
+        </div>
+      </div>
+
+      <div class="opinion-buttons">
+        <img src="${iconBack}" alt="Botón atrás carrusel opiniones">
+        <img src="${iconNext}" alt="Botón adelante carrusel opiniones">
+      </div>
+
+    </section>
+  </main>
+`;
+  }
+}  
+  
+  
+  
