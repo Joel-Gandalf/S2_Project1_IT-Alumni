e4ca7bcb2a411ab7b2e476aa3cb066b38a53cdc3
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-05-30 14:25:22 +0200

    refactor(nav-bar-laptop): replace duplicate nav-bar-profile with profile-options component

---
+++ src/components/nav-bar-laptop/profile-options.ts
@@ -0,0 +1,19 @@
+import './nav-bar.css';
+import searchIcon from "/src/assets/icons/Search.png";
+import bellIcon from "/src/assets/icons/NavBar-profile/bell.png";
+import gearIcon from "/src/assets/icons/NavBar-profile/settings-gear.png";
+import profilePhoto from "/src/assets/images/nav-bar-profile/profile-photo-4.png";
+
+export const createProfileOptions = (): Element | null => {
+  const profileOptions = document.createElement('div');
+
+  profileOptions.innerHTML = ` <div class="profile-options" aria-label="Opciones del perfil">
+          <a href="#"><img src="${searchIcon}" alt="Ir a busqueda"></a>
+          <a href="#"><img src="${bellIcon}" alt="Ir a notificaciones"></a>
+          <a href="#"><img src="${gearIcon}" alt="Ir a configuración"></a>
+          <a href="#" aria-label="Ir a editar perfil"><img src="${profilePhoto}" alt="Tú foto de perfil"></a>
+        </div>
+`;
+
+  return profileOptions.firstElementChild;
+}
