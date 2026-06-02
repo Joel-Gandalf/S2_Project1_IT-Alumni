51c9b7998be0db6ab648ef742c5a1a575b48db40
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-06-02 22:04:00 +0200

    feat(main-networking-laptop): Add laptop networking main CSS styles

--- src/components/main-networking-laptop/main-networking-laptop.ts
+++ src/components/main-networking-laptop/main-networking-laptop.ts
@@ -5,19 +5,23 @@ export const createMainNetworkingLaptop = (): Element | null => {
     const mainNetworkingLaptop = document.createElement('div');
     
     mainNetworkingLaptop.innerHTML = `
-    <section class="container-networking-filters">
-        <div class="networking-search-box" aria-label="Busqueda de alumnis por nombe">
-            <label><img src="${searchIcon}" alt="Icono lupa busqueda"><input id="networking-search" type="search" placeholder="Cercar alumni..."></label>
-        </div>
-        <div class="networking-filters" aria-label="Filtros de búsqueda">
-            <p>Filtres:</p>
-            <button data-filter="recent-activity" class="active" type="button">Activitat recent</button>
-            <button data-filter="popular" type="button">Popularitat</button>
-            <button data-filter="most-connected" type="button">Alta freqüència de connexió</button>
-        </div>
-    </section>
+    <main class="main-networking-laptop-container">
+        <section class="networking-filters-container">
+            <div class="networking-search-box" aria-label="Busqueda de alumnis por nombe">
+                <label><img src="${searchIcon}" alt="Icono lupa busqueda"><input id="networking-search" type="search" placeholder="Cercar alumni..."></label>
+            </div>
+            <div class="networking-filters">
+                <p>Filtres:</p>
+                <div class="networking-filters-box" aria-label="Filtros de búsqueda">
+                    <button data-filter="recent-activity" class="active" type="button">Activitat recent</button>
+                    <button data-filter="popular" type="button">Popularitat</button>
+                    <button data-filter="most-connected" type="button">Alta freqüència de connexió</button>
+                </div>
+            </div>
+        </section>
 
-    <section class="grid-insert-users"></section>
+        <section class="grid-insert-users"></section>
+    </main>
     `;
 
     const mainNetworkingLaptopElement = mainNetworkingLaptop.firstElementChild;
