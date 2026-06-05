639c5fdef81ccd07d49bbb1b59b5f0147cdc3940
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-06-05 22:57:59 +0200

    feat(networking-layout): Upgrade main-networking.css with responsive styles for laptop and mobile

--- src/components/main-networking/main-networking.ts
+++ src/components/main-networking/main-networking.ts
@@ -17,7 +17,7 @@ export const createMainNetworking = (isPhone: boolean = false): Element | null =
                 <div class="networking-filters-box" aria-label="Filtros de búsqueda">
                     <button data-filter="recent-activity" class="active" type="button">Activitat recent</button>
                     <button data-filter="popular" type="button">Popularitat</button>
-                    <button data-filter="most-connected" type="button">Alta freqüència de connexió</button>
+                    <button data-filter="most-connected" type="button">Connexió freqüent</button>
                 </div>
             </div>
         </section>
