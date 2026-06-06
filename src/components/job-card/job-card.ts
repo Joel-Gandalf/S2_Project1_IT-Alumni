dbada91f8138b593a545d032cb9875261d3a7acc
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-06-06 16:15:09 +0200

    feat(job-card): implement job card structure with relative date formatter and interface definitions

--- src/components/job-card/job-card.ts
+++ src/components/job-card/job-card.ts
@@ -1,62 +1,90 @@
 import './job-card.css';
 
-export interface AlumniData {
-        name: string,
-        surname: string,
-        jobPosition: string,
-        company: string,
-        city: string,
-        internationalCountryCodes: string,
-        classOf: number,
-        followers: number,
-        lastActivity: number,
-        NumberConnectionsLastMonth: number
+export interface JobData {
+        jobPosition: string;
+        employmentType: string;
+        datePosted: number;
+        company: string;
+        remote: boolean;
+        city: string;
+        internationalCountryCodes: string;
+        applicationDeadline: number;
+        jobLevel: 'Junior' | 'Semi-senior' | 'Senior';
+        sector: 'Software' | 'Disseny' | 'Marketing';
 }
 
-const formatter = new Intl.DateTimeFormat('es', {
+const formatterApplicationDeadline = new Intl.DateTimeFormat('ca', {
     month: 'short',
     day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit'
 });
 
-export const createAlumniCard = (student: AlumniData, isPhone: boolean = false): Element | null => {
-    const alumniCard = document.createElement('div');
+const formatterDatePosted = new Intl.RelativeTimeFormat('ca', {
+    numeric: 'always',
+    style: 'long'
+})
+
+const getRelativePostedTime = (timestampPosted: number): string => {
+
+    const NOW = new Date().getTime(); 
+    
+    const differenceInMs = timestampPosted - NOW;
+    
+    // Convertimos los milisegundos de diferencia a días enteros
+    const differenceInDays = Math.round(differenceInMs / (1000 * 60 * 60 * 24));
+
+    // Si la diferencia es menor de 7 días, devolvemos el formato en días
+    if (Math.abs(differenceInDays) < 7) {
+        return formatterDatePosted.format(differenceInDays, 'day');
+    }
+    
+    const differenceInWeeks = Math.round(differenceInDays / 7);
+    return formatterDatePosted.format(differenceInWeeks, 'week');
+};
+
+export const createJobCard = (job: JobData, isPhone: boolean = false): Element | null => {
+    const JobCard = document.createElement('div');
 
     if (isPhone) {
+        const formatDateDeadLine = formatterApplicationDeadline.format(job.applicationDeadline);
 
-        alumniCard.innerHTML = `
-            <article class="alumni-card alumni-card-phone" aria-label="Tarjeta de perfil de Alumni">
-                <div class="alumni-info">
-                    <h3>${student.name} ${student.surname}</h3>
-                    <p>Promoció ${student.classOf}</p>
-                    <p>${student.jobPosition} en ${student.company}</p>
-                    <p class="followers">Seguidors: ${student.followers}</p>
+        JobCard.innerHTML = `
+            <article class="job-card job-card-phone" aria-label="Tarjeta informativa de oferta laboral">
+                <div class="job-basic-container">
+                    <div class="job-info">
+                        <h3>${job.sector}</h3>
+                        <p>${job.jobPosition} a ${job.company}</p>
+                        ${job.remote ? `<p>Remote | Caduca ${formatDateDeadLine}</p>` : `<p>${job.city} | Caduca ${formatDateDeadLine}</p>`}
+                    </div>
+
+                    <button class="button-details" type="button" aria-label="Ver más información de la oferta de ${job.jobPosition}">Veure detalls</button>
                 </div>
-
-                <div class="alumni-image-profile" role="img" aria-label="Imagen de perfil de ${student.name}"></div>
+                
+                <div class="job-image-company" role="img" aria-label="Imagen representativa de ${job.company}"></div>
             </article>
         `;
         
-        return alumniCard.firstElementChild;
+        return JobCard.firstElementChild;
     }
 
-    const formatDate = formatter.format(student.lastActivity);
+    const relativeTimeText = getRelativePostedTime(job.datePosted);
 
-    alumniCard.innerHTML = `
-        <article class="alumni-card alumni-card-laptop" aria-label="Tarjeta de perfil de Alumni">
-            <div class="alumni-info">
-                <h3>${student.name} ${student.surname}</h3>
-                <p>Promoció ${student.classOf}</p>
-                <p>${student.jobPosition} en ${student.company}</p>
-                <p>${student.city}, ${student.internationalCountryCodes}</p>
-                <p class="followers">Seguidors: ${student.followers}</p>
-                <p>Última connexió: ${formatDate}</p>
+    JobCard.innerHTML = `
+        <article class="job-card job-card-laptop" aria-label="Tarjeta informativa de oferta laboral">
+            <div class="job-basic-container">
+                <div class="job-info">
+                    <h3>${job.sector}</h3>
+                    <p>${job.jobPosition} a ${job.company}</p>                   
+                    <p>${job.employmentType}</p>
+                    ${job.remote ? `<p>Remote</p>` : `<p>${job.city} ${job.internationalCountryCodes}</p>`} 
+                    <p>Publicat ${relativeTimeText}</p>
+                </div>
+                
+                <button class="button-details" type="button" aria-label="Ver más información de la oferta de ${job.jobPosition}">Veure detalls</button>            
             </div>
 
-            <button class="button-message" type="button" aria-label="Enviar mensaje a ${student.name}">Missatge</button>
+            <div class="job-image-company" role="img" aria-label="Imagen representativa de ${job.company}"></div>
         </article>
     `;
 
-    return alumniCard.firstElementChild;
+    return JobCard.firstElementChild;
 }
